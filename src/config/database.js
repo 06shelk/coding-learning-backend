63fa4b58ef2f8f26f51e6ac2f7c00eee14db5f66
06shelk <shlee4349@gmail.com>
2024-09-29 00:41:26 +0900

    .

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
-// const mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 
-// // MySQL 데이터베이스 연결 설정
-// const db = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'mysqlP@ssword',
-//     database: 'coding_learning'
-// });
+// MySQL 데이터베이스 연결 설정
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'mysqlP@ssword',
+    database: 'coding_learning'
+});
 
-// module.exports = db;
+module.exports = db;
